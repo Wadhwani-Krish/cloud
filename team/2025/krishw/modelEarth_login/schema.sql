c7b1c1e6c19cb7013043f0316871830e53cbfb1a
Wadhwani-Krish <krishwadhwani575@gmail.com>
2025-02-24 12:16:00 -0600

    Update schema.sql

--- team/2025/krishw/modelEarth_login/schema.sql
+++ team/2025/krishw/modelEarth_login/schema.sql
@@ -6,7 +6,5 @@ CREATE TABLE user (
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN DEFAULT FALSE,
-    totp_secret VARCHAR(16) NULL,
     PRIMARY KEY (id)
 );
